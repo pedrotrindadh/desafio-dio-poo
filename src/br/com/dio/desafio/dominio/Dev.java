7878d930bcd9b91335dd5f6c4249f1bbe317342b
Pedro Trindade <prindade@outlook.com>
2026-01-06 12:10:50 -0300

    Bootcamp e Dev

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,61 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+	private String nome;
+	private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
+	
+	public void inscreverBootcamp(Bootcamp bootcamp) {}
+	
+	public void progredir() {}
+	
+	public void calcularTotalXp() {}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Conteudo> getConteudoInscritos() {
+		return conteudoInscritos;
+	}
+
+	public void setConteudoInscritos(Set<Conteudo> conteudoInscritos) {
+		this.conteudoInscritos = conteudoInscritos;
+	}
+
+	public Set<Conteudo> getConteudoConcluidos() {
+		return conteudoConcluidos;
+	}
+
+	public void setConteudoConcluidos(Set<Conteudo> conteudoConcluidos) {
+		this.conteudoConcluidos = conteudoConcluidos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conteudoConcluidos, conteudoInscritos, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dev other = (Dev) obj;
+		return Objects.equals(conteudoConcluidos, other.conteudoConcluidos)
+				&& Objects.equals(conteudoInscritos, other.conteudoInscritos) && Objects.equals(nome, other.nome);
+	}
+	
+	
+}
